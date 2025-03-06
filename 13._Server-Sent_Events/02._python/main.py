75e54fecb6e6074ddf510ea4a32f2395b499a538
Lasse Rugaard Rasmussen <lass531n@stud.kea.dk>
2025-03-06 10:22:21 +0100

    class exercise added

--- 13._Server-Sent_Events/02._python/main.py
+++ 13._Server-Sent_Events/02._python/main.py
@@ -1,3 +1,5 @@
+import time
+
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import StreamingResponse
@@ -18,9 +20,20 @@ async def date_generator():
         now = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
         yield f"data: {now}\n\n"
         await asyncio.sleep(1)
+        
+def count_and_print():
+    for i in range(1, 6):
+        yield f"data: {i}\n\n"
+        time.sleep(1)
+    yield("this assignment is epic")
 
 
 @app.get("/sse")
 def sse():
     return StreamingResponse(date_generator(), media_type="text/event-stream")
+
+@app.get("/epic")
+def epic():
+    return StreamingResponse(count_and_print(), media_type="text/event-stream")
+
 
